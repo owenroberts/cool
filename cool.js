18b50a6da98b631b6661e313665607a527d6b059
Owen Roberts <obroberts@gmail.com>
2025-06-24 12:22:50 -0400

    adds assert, random fixes

--- cool.js
+++ cool.js
@@ -24,6 +24,19 @@ if (typeof window !== 'undefined') {
 	})();
 }
 
+/**
+ * throw error if assertion is false in dev mode
+ * tied to vite environment
+ * @param  {boolean} condition 
+ * @param  {string} message - print with error
+ * @throws {Error} If assertion fails
+ */
+function assert(condition, message) {
+	if (import.meta.env.DEV && !condition) {
+		throw new Error(`assertion failed: ${ message || "No error message" }`);
+	}
+}
+
 function getDate() {
 	return new Date().toDateString().replace(/ /g, '-');
 }
@@ -55,17 +68,30 @@ function random(min, max) {
 	}
 }
 
-function randomInt(min, max) {
+function randomInt(min, max, maxInclusive=true) {
+	assert(Number.isFinite(min), "min is not a number");
 	if (!max) max = min, min = 0;
-	return Math.round( Math.random() * (max - min) + min );
+	assert(Number.isFinite(max), "max is not a number");
+	return maxInclusive ? 
+		Math.round(Math.random() * (max - min) + min) :
+		Math.floor(Math.random() * (max - min) + min);
 }
 
 const randInt = randomInt;
 
+/**
+ * random chance 0-1
+ * @param  {number} n 0-1 representing %
+ * @return {boolean}
+ */
 function chance(n) {
-	return random(1) < n;
+	return Math.random() < n;
 }
 
+/**
+ * random 50% true or false
+ * @return {boolean}
+ */
 function coinFlip() {
 	return chance(0.5);
 }
@@ -75,12 +101,17 @@ function choice(choices) {
 	return choices[Math.floor(Math.random() * choices.length)];
 }
 
+/**
+ * suffles items in array
+ * based on https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * @param  {Array} array
+ * @return {Array} shuffled     
+ */
 function shuffle(array) {
-  // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-  let currentIndex = array.length, randomIndex;
+	let currentIndex = array.length, randomIndex;
 
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
+	// While there remain elements to shuffle...
+	while (currentIndex != 0) {
 
 	// Pick a remaining element...
 	randomIndex = Math.floor(Math.random() * currentIndex);
@@ -89,9 +120,9 @@ function shuffle(array) {
 	// And swap it with the current element.
 	[array[currentIndex], array[randomIndex]] = [
 	  array[randomIndex], array[currentIndex]];
-  }
+	}
 
-  return array;
+	return array;
 }
 
 // https://stackoverflow.com/a/36481059
@@ -221,11 +252,11 @@ function rgbToHsl(rgb) {
 
 // http://wowmotty.blogspot.com/2009/06/convert-jquery-rgb-output-to-hex-color.html
 function rgb2hex(orig) {
- var rgb = orig.replace(/\s/g,'').match(/^rgba?\((\d+),(\d+),(\d+)/i);
- return (rgb && rgb.length === 4) ? "#" +
-  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : orig;
+	var rgb = orig.replace(/\s/g,'').match(/^rgba?\((\d+),(\d+),(\d+)/i);
+	return (rgb && rgb.length === 4) ? "#" +
+		("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+		("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+		("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : orig;
 }
 
 /* vector stuff */
@@ -340,12 +371,6 @@ function Vector(x,y) {
 };
 
 /* browser stuff */
-function mobilecheck() {
-	var check = false;
-	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-	return check;
-}
-
 function testPerformance() {
 	// https://stackoverflow.com/questions/19754792/measure-cpu-performance-via-js
 	var _speedconstant = 1.15600e-8; //if speed=(c*a)/t, then constant=(s*t)/a and time=(a*c)/s
@@ -382,15 +407,27 @@ function testLowPerformance(threshold=0.1) {
 	return average > threshold; // true means low performance
 }
 
-/* a counter */
-function Counter(duration=24, loop=false, callback) {
-	
+// should be a class (or createCounter)
+/**
+ * A counter for counting
+ * setLoop(true) to loop
+ * @param {Number}   duration (in frames)
+ * @param {Function} callback (callback on end);
+ * @return {Object} update, reset, isDone(), setLoop(), end(), getProgress(), getCount(), setCount(), getDuration(), setDuration()
+ */
+function Counter(duration=24, callback) {
+
 	let count = 0;
+	let loop = false;
 	let isDone = false;
 
+	/**
+	 * count up by one
+	 * @param  {Number} timeMod should change this to like pass params or something
+	 */
 	function update(timeMod=1) {
 		if (count >= duration) {
-			if (callback) callback(timeMod);
+			if (!isDone && callback) callback(timeMod);
 			if (loop) {
 				reset();
 			} else {
@@ -399,15 +436,20 @@ function Counter(duration=24, loop=false, callback) {
 		} else {
 			count = count + 1;
 		}
+		return count;
 	}
 
+	/**
+	 * reset the count to 0
+	 */
 	function reset() {
 		count = 0;
 	}
 
 	return {
 		update, reset,
-		isDone: () => { return isDone; },
+		setLoop: value => { loop = value; },
+		isDone: () => { return count >= duration; },
 		end: () => { count = duration; },
 		getProgress: () => { return count / duration },
 		getCount: () => { return count; },
@@ -441,25 +483,44 @@ function CounterSequence(sequence=[]) {
 	return { add, update, next, stop };
 }
 
+/**
+ * simple sequencer - add callbacks and call them in sequence
+ */
 function Sequencer() {
+	
 	let sequence = [];
+	let isOver = false;
 
+	/**
+	 * add a callback
+	 * @param {Function} callback 
+	 */
 	function add(callback) {
 		sequence.push({ callback });
 	}
 
+	/**
+	 * go to next callback
+	 */
 	function next() {
-		sequence.shift();
+		if (sequence.length > 0) {
+			let n = sequence.shift();
+			n.callback();
+		} else if (!isOver) {
+			isOver = true;
+			console.warn('Sequence over.');
+		}
 	}
 
-	function update() {
-		sequence[0].callback();
+	/**
+	 * is the sequence done?
+	 * @returns {boolean} isDone
+	 */
+	function isDone() { 
+		return sequence.length === 0; 
 	}
 
-	return {
-		add, next, update,
-		isDone: () => { return sequence.length === 0; },
-	};
+	return { add, next, isDone };
 }
 
 function CounterSequencer() {
@@ -492,16 +553,19 @@ function CounterSequencer() {
 }
 
 
-/* ascii key mapping */
+/**
+ * ascii key map
+ * @type {Object}
+ */
 const keys = {
-	"9":   "tab",
-	"13":  "enter",
-	"27":  "escape",
-	"32":  "space",
-	"37":  "left",
-	"38":  "up",
-	"39":  "right",
-	"40":  "down",
+	"9": "tab",
+	"13": "enter",
+	"27": "escape",
+	"32": "space",
+	"37": "left",
+	"38": "up",
+	"39": "right",
+	"40": "down",
 	"48": "0",
 	"49": "1",
 	"50": "2",
@@ -512,32 +576,32 @@ const keys = {
 	"55": "7",
 	"56": "8",
 	"57": "9",
-	"65":  "a",	
-	"66":  "b",
-	"67":  "c",
-	"68":  "d",
-	"69":  "e",
-	"70":  "f",
-	"71":  "g",
-	"72":  "h",
-	"73":  "i",
-	"74":  "j",
-	"75":  "k",
-	"76":  "l",
-	"77":  "m",
-	"78":  "n",
-	"79":  "o",
-	"80":  "p",
-	"81":  "q",
-	"82":  "r",
-	"83":  "s",
-	"84":  "t",
-	"85":  "u",
-	"86":  "v",
-	"87":  "w",
-	"88":  "x",
-	"89":  "y",
-	"90":  "z",
+	"65": "a",	
+	"66": "b",
+	"67": "c",
+	"68": "d",
+	"69": "e",
+	"70": "f",
+	"71": "g",
+	"72": "h",
+	"73": "i",
+	"74": "j",
+	"75": "k",
+	"76": "l",
+	"77": "m",
+	"78": "n",
+	"79": "o",
+	"80": "p",
+	"81": "q",
+	"82": "r",
+	"83": "s",
+	"84": "t",
+	"85": "u",
+	"86": "v",
+	"87": "w",
+	"88": "x",
+	"89": "y",
+	"90": "z",
 	"186": ";",
 	"187": "+",
 	"188": ",",
@@ -550,13 +614,16 @@ const keys = {
 	"222": "'",
 };
 
-// 8 bit bit mask auto tile order for 48 tiles
-// https://unit.dev/posts/blobmaps/
-const tileMap = [
-	0, 4, 68, 64, 117, 71, 197, 93, 7, 199, 215, 193,
-	1, 5, 69, 65, 23, 223, 247, 209, 95, 255, 221, 241,
-	17, 21, 85, 81, 29, 127, 253, 113, 31, 119, 'x', 245,
-	16, 20, 84, 80, 219, 92, 116, 87, 28, 125, 124, 112,
-];
+/**
+ * check for mobile browser
+ * from http://detectmobilebrowsers.com/
+ * @return {boolean} true if is mobile
+ */
+function mobilecheck() {
+	var check = false;
+	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	return check;
+}
 
-export { getDate, map, padNumber, random, randomInt, chance, coinFlip, choice, shuffle, randomNormalInverse, componentToHex, HueToRgb, hslToHex, hexToRgb, rgbToHsl, rgb2hex, Vector, mobilecheck, testPerformance, testLowPerformance, keys, tileMap, randInt, Counter, Sequencer, CounterSequence };
+
+export { getDate, map, padNumber, random, randomInt, chance, coinFlip, choice, shuffle, randomNormalInverse, componentToHex, HueToRgb, hslToHex, hexToRgb, rgbToHsl, rgb2hex, Vector, mobilecheck, testPerformance, testLowPerformance, keys, randInt, Counter, Sequencer, CounterSequence, assert };
